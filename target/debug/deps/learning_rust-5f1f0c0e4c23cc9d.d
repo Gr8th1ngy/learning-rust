4b535e8388b032f022c8256508219c4809a3ce2c
Gr8th1ngy <anhhuyapmops@gmail.com>
2018-09-16 17:00:25 -0400

    Initial commit

---
+++ target/debug/deps/learning_rust-5f1f0c0e4c23cc9d.d
@@ -0,0 +1,5 @@
+/home/gr8th1ngy/eclipse-workspace/learning-rust/target/debug/deps/learning_rust-5f1f0c0e4c23cc9d: src/main.rs
+
+/home/gr8th1ngy/eclipse-workspace/learning-rust/target/debug/deps/learning_rust-5f1f0c0e4c23cc9d.d: src/main.rs
+
+src/main.rs:
